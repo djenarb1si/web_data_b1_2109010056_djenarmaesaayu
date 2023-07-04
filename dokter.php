bc00d591a58a6e686122c189affef0cd041e28dc
djenarb1si <136410185+djenarb1si@users.noreply.github.com>
2023-07-04 09:46:25 +0700

    Add files via upload

---
+++ dokter.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edhe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+    <h1>ini adalalah Halaman Dokter</h1>
+    <ul>
+        <a href="index.php">
+            <li>Home</li>
+        </a>
+        <a href="Jadwal_Dokter.php">
+            <li>Jadwal Dokter</li>
+        </a>
+    </ul>
+    <hr>
+    <a href="tambah_dokter.php">Tambah Data Dokter</a>
+</body>
+</html>
